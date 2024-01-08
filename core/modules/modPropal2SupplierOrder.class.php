28b7f7c5106488268e919e4b6e19d088347c1fc8
John BOTELLA <john.botella@atm-consulting.fr>
2024-01-08 17:01:04 +0100

    Release 1.3

--- core/modules/modPropal2SupplierOrder.class.php
+++ core/modules/modPropal2SupplierOrder.class.php
@@ -52,13 +52,18 @@ class modPropal2SupplierOrder extends DolibarrModules
 
 		// Family can be 'crm','financial','hr','projects','products','ecm','technic','other'
 		// It is used to group modules in module setup page
-		$this->family = "ATM Consulting";
+		$this->family = "ATM Consulting - CRM";
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i','',get_class($this));
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Description of module Propal2SupplierOrder";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '1.3.0';
+		$this->version = '1.4.0';
+
+		// Url to the file with your last numberversion of this module
+		require_once __DIR__ . '/../../class/techatm.class.php';
+		$this->url_last_version = \propal2supplierorder\TechATM::getLastModuleVersionUrl($this);
+
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
